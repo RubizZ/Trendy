5a25328f79115651ae3b9b5911b186f6bb2f70a7
rouu04 <rociunon@ucm.es>
2024-04-21 12:57:43 +0200

    Hacer el SA de las listas Co-authored-by: Irene <ivalve02@ucm.es>

--- src/model/articulo/SAListas.java
+++ src/model/articulo/SAListas.java
@@ -1,4 +1,7 @@
 package model.articulo;
 
+import java.util.List;
+
 public interface SAListas {
+    public List<Articulo> buscaArticulosCategoria(String cat);
 }
--- src/model/articulo/SAListasImp.java
+++ src/model/articulo/SAListasImp.java
@@ -3,13 +3,27 @@ package model.articulo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SAListasImp {
+public class SAListasImp implements SAListas{
+
+    //PLAN
+    /*
+
+    Cogemos las listas de la tabla de categorias join arituclo -> las listas son de articulo
+    Luego al mostrar un artículo (en view) llamaremos a la tabla para ver su fecha lanzamiento y descuento
+
+
+     */
+
+
 
     //SON DE BUSINESS OBJECT
     List<Articulo> listaMujer;
     List<Articulo> listaHombre;
     List<Articulo> listaExclusivos;
     List<Articulo> listaPromociones;
+
+    DAOListas dao = new DAOListasImp();
+    DAOArticulo daoArt = new DAOArticuloImp();
 
     public SAListasImp(){
         listaMujer=new ArrayList<>();
@@ -18,16 +32,31 @@ public class SAListasImp {
         listaPromociones=new ArrayList<>();
     }
 
-    public void anadir(SAArticulo art, String cat){
-        switch (cat to categoria){
-            case MUJER -> listaMujer.add(art);
-            case HOMBRE -> listaHombre.add(art);
-            case EXCLUSIVOS -> listaExclusivos.add(art);
+    private List<Articulo> buscaLista(String cat){
+        switch (cat.toUpperCase()){
+            case "MUJER":
+                return this.listaMujer;
+            case "HOMBRE":
+                return this.listaHombre;
+            case "EXCLUSIVOS":
+                return this.listaExclusivos;
+            case "PROMOCIONES":
+                return this.listaPromociones;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public List<Articulo> buscaArticulosCategoria(String cat) {
+        List<Integer> tLista = dao.buscaArticulosCategoria(cat);
+
+        List<Articulo> lista = buscaLista(cat);
+
+        for(int id : tLista){
+            lista.add(new Articulo(daoArt.buscarArticulo(id)));
         }
 
-        //yo lo meteria directamente
-        if(art.getDescuento() > 0){
-            listaPromociones.add(art);
-        }
+        return lista;
     }
 }
