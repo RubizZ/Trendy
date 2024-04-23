3c7db4962383218ee83913863c24c5921f34309b
RAMIRO <ramimarc@ucm.es>
2024-04-23 03:53:25 +0200

    SA de cesta

---
+++ src/model/ventas/SACesta.java
@@ -0,0 +1,12 @@
+package model.ventas;
+
+public interface SACesta {
+    void addCesta(TOCesta tOcesta);
+
+    TOCesta readCesta(int id);
+
+    void addArticuloACesta(TOCesta tOcesta);
+
+    TOCesta readArticuloEnCesta(int id_cesta, int id_articulo);
+
+}
---
+++ src/model/ventas/SACestaimp.java
@@ -0,0 +1,27 @@
+package model.ventas;
+
+
+
+public class SACestaimp implements SACesta{
+
+    private DAOCesta daoCesta = new DAOCestaimp();
+    @Override
+    public void addCesta(TOCesta tOcesta) {
+        daoCesta.añadirCesta(tOcesta);
+    }
+
+    @Override
+    public TOCesta readCesta(int id) {
+        return daoCesta.getCesta(id);
+    }
+
+    @Override
+    public void addArticuloACesta(TOCesta tOcesta) {
+        daoCesta.añadirArticuloACesta(tOcesta);
+    }
+
+    @Override
+    public TOCesta readArticuloEnCesta(int id_cesta, int id_articulo) {
+        return daoCesta.getArticuloEnCesta(id_cesta, id_articulo);
+    }
+}
