3a187d52721552ddfd596a8506c4cd9e8f0778b2
Rubén <45830465+RubizZ@users.noreply.github.com>
2024-04-23 23:49:15 +0200

    Declaración de metodos de BusinessDelegate
    Co-Authored-By: JaviMNogal <146330751+JaviMNogal@users.noreply.github.com>

--- src/negocio/BusinessDelegate.java
+++ src/negocio/BusinessDelegate.java
@@ -1,4 +1,34 @@
 package negocio;
 
+import java.util.Collection;
+import java.util.Date;
+
 public class BusinessDelegate {
+    public void crearPedido() {
+        //TODO
+    }
+
+    public Collection<TOPedido> getAllPedidos() {
+        return null; //TODO
+    }
+
+    public Collection<TOPedido> getPedidosUsuario(int idUsuario) {
+        return null; //TODO
+    }
+
+    public Collection<TOPedido> getPedidosStatus(TOStatusPedido toStatusPedido) {
+        return null; //TODO
+    }
+
+    public Collection<TOPedido> getPedidosFecha(Date fecha) {
+        return null; //TODO
+    }
+
+    public void cambiarStatus(int id, TOStatusPedido toStatusPedido) {
+        //TODO
+    }
+
+    public void cancelarPedido(int id) {
+        //TODO
+    }
 }
