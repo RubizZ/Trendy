887be24756878e1b25ef18c8d2624eb252ad5746
Jaavii16 <javdelol@ucm.es>
2024-04-30 17:10:19 +0200

    Cambio de la interfaz de usuario a un CardPanel
    Co-Authored-By: aibanarias <131764097+aibanarias@users.noreply.github.com>

--- src/presentacion/GUIPerfil.java
+++ src/presentacion/GUIPerfil.java
@@ -7,6 +7,8 @@ import utils.ViewUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
 
 public class GUIPerfil extends MainGUIPanel {
@@ -22,80 +24,109 @@ public class GUIPerfil extends MainGUIPanel {
     private JTextField _pais;
     private JTextField _anyoNac;
     private JTextField _direccion;
-    private JPanel mainPanel;
+
+    private JPanel cards;
+    private static final String PANELINI = "Panel_ini";
+    private static final String PANELMOD = "Panel_mod";
+    private static final String PANELPEDIDOS = "Panel_ped";
+    private static final String PANELSUSCR = "Panel_susc";
+    private static final String PANELSALDO = "Panel_saldo";
 
     public GUIPerfil(SAFacade facade){
         saUsuario = facade;
         initGUI();
     }
 
+    class VentanaMensaje extends JFrame {
+        public VentanaMensaje(String mensaje) {
+            JLabel etiqueta = new JLabel(mensaje);
+            getContentPane().add(etiqueta);
+            setSize(200, 100);
+            setVisible(true);
+        }
+    }
+
     private void initGUI() {
-        mainPanel = new JPanel();
+
+        cards = new JPanel(new CardLayout());
+
+        //CREAMOS LOS PANELES
+        //PANEL DE MODIFICAR DATOS
+        JPanel panelMod = new JPanel();
+        panelMod.setLayout(new BoxLayout(panelMod, BoxLayout.Y_AXIS));
+        panelMod.setVisible(false);
+        configurarPanelMod(cards, panelMod);
+
+        //PANEL DE VER MIS PEDIDOS
+        JPanel panelPedidos = new JPanel();
+        panelPedidos.setLayout(new BoxLayout(panelPedidos, BoxLayout.Y_AXIS));
+        panelPedidos.setVisible(false);
+        configurarPanelPedidos(panelPedidos);
+
+        //PANEL DE ACTUALIZAR SUSCRIPCION
+        JPanel panelSuscr = new JPanel();
+        panelSuscr.setLayout(new BoxLayout(panelSuscr, BoxLayout.Y_AXIS));
+        panelSuscr.setVisible(false);
+        configurarPanelSuscr(cards, panelSuscr);
+
+        //PANEL DE AÑADIR SALDO
+        JPanel panelSaldo = new JPanel();
+        panelSaldo.setLayout(new BoxLayout(panelSaldo, BoxLayout.Y_AXIS));
+        panelSaldo.setVisible(false);
+        configurarPanelSaldo(cards, panelSaldo);
 
         //PANEL QUE SE VA A MOSTRAR AL PRINCIPIO
         JPanel panelIni = new JPanel();
-        panelIni.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        panelIni.setLayout(new BoxLayout(panelIni, BoxLayout.Y_AXIS));
         JLabel nombre = new JLabel(saUsuario.getUsuario().getNombre() + " " +
                 saUsuario.getUsuario().getApellidos());
         panelIni.add(nombre);
 
-        //PANEL DE MODIFICAR DATOS
-        JPanel panelMod = new JPanel();
-        panelMod.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        panelMod.setVisible(false);
-        configurarPanelMod(panelIni, panelMod);
 
-        //PANEL DE VER MIS PEDIDOS
-        JPanel panelPedidos = new JPanel();
-        panelPedidos.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        panelPedidos.setVisible(false);
-        configurarPanelPedidos(panelIni, panelPedidos);
-
-        //PANEL DE ACTUALIZAR SUSCRIPCION
-        JPanel panelSuscr = new JPanel();
-        panelSuscr.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        panelSuscr.setVisible(false);
-        configurarPanelSuscr(panelIni, panelSuscr);
+        // Añadir paneles al panel de cartas
+        cards.add(panelIni, PANELINI);
+        cards.add(panelMod, PANELMOD);
+        cards.add(panelPedidos, PANELPEDIDOS);
+        cards.add(panelSuscr, PANELSUSCR);
+        cards.add(panelSaldo, PANELSALDO);
 
 
-        //PANEL DE AÑADIR SALDO
-        JPanel panelSaldo = new JPanel();
-        panelSaldo.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        panelSaldo.setVisible(false);
-        configurarPanelSaldo(panelIni,panelSaldo);
+        //PARA CAMBIAR LOS PANELES CON LOS ACTION LISTENERS
+        CardLayout cl = (CardLayout)(cards.getLayout());
+
+        //PANEL PARA AÑADIR LOS BOTONES
+        JPanel buttonPanel = new JPanel();
+
+        //BOTON PARA VOLVER AL PANEL INICIAL
+        JButton ini = new JButton("Inicio");
+        buttonPanel.add(ini);
+        ini.addActionListener((e -> cl.show(cards,"Panel_ini")));
 
 
         //BOTON PARA MODIFICAR DATOS
         JButton mod_datos = new JButton("Modificar datos");
-        mod_datos.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panelIni.add(mod_datos);
-        mod_datos.addActionListener((e -> {
-            panelIni.setVisible(false);
-            panelMod.setVisible(true);
-        }));
+        buttonPanel.add(mod_datos);
+        mod_datos.addActionListener((e -> cl.show(cards, "Panel_mod")));
 
         //BOTON PARA VER MIS PEDIDOS
         JButton ver_pedidos = new JButton("Ver mis pedidos");
-        ver_pedidos.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panelIni.add(ver_pedidos);
-        ver_pedidos.addActionListener((e -> {
-            panelIni.setVisible(false);
-            panelPedidos.setVisible(true);
-        }));
+        buttonPanel.add(ver_pedidos);
+        ver_pedidos.addActionListener((e -> cl.show(cards, "Panel_ped")));
 
         //BOTON PARA ACTUALIZAR LA SUSCRIPCION
         JButton act_suscripcion = new JButton("Actualizar suscripcion");
-        act_suscripcion.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panelIni.add(act_suscripcion);
-        act_suscripcion.addActionListener((e -> {
-            panelIni.setVisible(false);
-            panelSuscr.setVisible(true);
-        }));
+        buttonPanel.add(act_suscripcion);
+        act_suscripcion.addActionListener((e ->cl.show(cards, "Panel_susc")));
 
         //BOTON PARA AÑADIR SALDO
         JButton add_saldo = new JButton("Añadir saldo");
-        add_saldo.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panelIni.add(add_saldo);
+        buttonPanel.add(add_saldo);
+        add_saldo.addActionListener((e -> cl.show(cards, "Panel_saldo")));
+
+
+        // Agrega los paneles y el panel de botones al panel principal
+        getContentPane().add(cards, BorderLayout.CENTER);
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
     }
 
 
@@ -106,9 +137,9 @@ public class GUIPerfil extends MainGUIPanel {
     }
 
 
-    private void configurarPanelMod(Container mainPanel, Container modPanel){
+    private void configurarPanelMod(JPanel cards ,JPanel modPanel){
         JTextArea texto_ayuda = new JTextArea("Complete todos los campos aunque no los quiera cambiar");
-        texto_ayuda.setBackground(getForeground());
+        texto_ayuda.setBackground(modPanel.getBackground());
         texto_ayuda.setLineWrap(true);
         texto_ayuda.setWrapStyleWord(true);
         texto_ayuda.setEditable(false);
@@ -150,11 +181,10 @@ public class GUIPerfil extends MainGUIPanel {
         modPanel.add(_direccion);
 
 
-
+        CardLayout cl = (CardLayout)(cards.getLayout());
         JButton _cancel = new JButton("Cancelar");
         _cancel.addActionListener((e) -> {
-            modPanel.setVisible(false);
-            mainPanel.setVisible(true);
+            cl.show(cards, "Panel_ini");
         });
         _cancel.setAlignmentX(Component.CENTER_ALIGNMENT);
         modPanel.add(_cancel);
@@ -164,29 +194,28 @@ public class GUIPerfil extends MainGUIPanel {
             TUsuario usuario = crearUsuario();
             try{
                 saUsuario.update(usuario);
+                VentanaMensaje ventana = new VentanaMensaje("Los datos se han modificado correctamente");
             }catch(RuntimeException re){
                 throw new RuntimeException("No se han podido actualizar los datos: ");
             }finally{
-                modPanel.setVisible(false);
-                mainPanel.setVisible(true);
+                cl.show(cards, "Panel_ini");
             }
         });
         _confirmar.setAlignmentX(Component.CENTER_ALIGNMENT);
         modPanel.add(_confirmar);
     }
 
-    private void configurarPanelPedidos(JPanel mainPanel, JPanel panelPedidos) {
+    private void configurarPanelPedidos( JPanel panelPedidos) {
         //TODO creo que solo tengo que hacer una instancia de la tabla de pedidos que tienen
         //que crear javi y ruben
-        JButton atras = new JButton("Atras");
+        /*JButton atras = new JButton("Atras");
         panelPedidos.add(atras);
         atras.addActionListener((e -> {
-            mainPanel.setVisible(true);
-            panelPedidos.setVisible(false);
-        }));
+            cl.show(cards, "Panel_ini");
+        }));*/
     }
 
-    private void configurarPanelSaldo(JPanel mainPanel, JPanel panelSaldo) {
+    private void configurarPanelSaldo(JPanel cards, JPanel panelSaldo) {
         JLabel cant = new JLabel("Introduzca la cantidad que desea añadir");
         cant.setAlignmentX(Component.CENTER_ALIGNMENT);
         panelSaldo.add(cant);
@@ -195,24 +224,25 @@ public class GUIPerfil extends MainGUIPanel {
         sumarASaldo.setAlignmentX(Component.CENTER_ALIGNMENT);
         panelSaldo.add(sumarASaldo);
 
+        CardLayout cl = (CardLayout)(cards.getLayout());
+
         JButton confirmar = new JButton("Confirmar");
         confirmar.setAlignmentX(Component.CENTER_ALIGNMENT);
         panelSaldo.add(confirmar);
         confirmar.addActionListener((e -> {
             int cantidad = (int) sumarASaldo.getValue();
             saUsuario.actualizarSaldo(cantidad);
-            mainPanel.setVisible(true);
-            panelSaldo.setVisible(false);
+            VentanaMensaje ventanaMensaje = new VentanaMensaje("Saldo añadido con éxito!");
+            cl.show(cards, "Panel_ini");
         }));
 
         JButton atras = new JButton("Atras");
         panelSaldo.add(atras);
         atras.addActionListener((e -> {
-            mainPanel.setVisible(true);
-            panelSaldo.setVisible(false);
+            cl.show(cards, "Panel_ini");
         }));
     }
-    private void configurarPanelSuscr(JPanel panelIni, JPanel panelSuscr) {
+    private void configurarPanelSuscr(JPanel cards, JPanel panelSuscr) {
         JLabel mensaje = new JLabel("Elija la suscripcion que desea:");
         mensaje.setAlignmentX(Component.CENTER_ALIGNMENT);
         panelSuscr.add(mensaje);
@@ -224,15 +254,14 @@ public class GUIPerfil extends MainGUIPanel {
         comboBoxSusc.setAlignmentX(Component.CENTER_ALIGNMENT);
         panelSuscr.add(comboBoxSusc);
 
+        CardLayout cl = (CardLayout)(cards.getLayout());
+
         JButton confirmar = new JButton("Confirmar");
         panelSuscr.add(confirmar);
-        confirmar.addActionListener((e -> saUsuario.actualizarSuscr(comboBoxSusc.getSelectedIndex())));
-
-        JButton atras = new JButton("Atras");
-        panelSuscr.add(atras);
-        atras.addActionListener((e -> {
-            mainPanel.setVisible(true);
-            panelIni.setVisible(false);
+        confirmar.addActionListener((e -> {
+            saUsuario.actualizarSuscr(comboBoxSusc.getSelectedIndex());
+            VentanaMensaje ventanaMensaje = new VentanaMensaje("Suscripcion actualizada con éxito!");
+            cl.show(cards, "Panel_ini");
         }));
     }
 
@@ -252,4 +281,6 @@ public class GUIPerfil extends MainGUIPanel {
         sexo = saUsuario.getUsuario().getSexo();
         return new TUsuario(nombre, apellidos, correo, contrasenya, anyo, sexo, pais, dir);
     }
+
+
 }
